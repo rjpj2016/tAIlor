68d24cc48c0641f8617c59cd73138e9d8a9658ec
Rajat Parajuli <rjpj@parajuli.me>
2020-05-06 19:45:31 +0545

    Added dummy parameters selector

--- src/pages/sir.js
+++ src/pages/sir.js
@@ -1,4 +1,4 @@
-import { Row,Col, Button } from "antd";
+import { Row,Col, Button, Select, Slider } from "antd";
 import { useEffect } from "react";
 import React, {useState} from 'react';
 import * as THREE from "three"
@@ -8,6 +8,9 @@ import Img from 'gatsby-image';
 import {RobotOutlined,UploadOutlined} from '@ant-design/icons';
 
 OBJLoader(THREE);
+
+
+const {Option} = Select;
 
 export default ({data}) => {
 
@@ -25,8 +28,8 @@ export default ({data}) => {
         // Make camera
         let near =0.1;
 	    let far=10000;
-        let fov=90;
-        let radiusOfCamera=310;
+        let fov=45;
+        let radiusOfCamera=700;
         let heightOfCamera=225;
         let aspect = domRef.offsetWidth/domRef.offsetHeight;
         let camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
@@ -158,8 +161,58 @@ export default ({data}) => {
                     </div>
                 </Col>
                 <Col xs={0} md={6} xl={5}>
-                    <div style={{height:"100%",width:"100%",backgroundColor:"#F5F5DC",display:"flex",justifyContent:"center"}}>
-                        <h1>Model</h1>
+                    <div style={{height:"100%",width:"100%",backgroundColor:"#F5F5DC"}}>
+                        <Row style={{display:"flex","justifyContent":"center"}}>
+                            <h1 style={{margin:"auto"}}>Models</h1>
+                        </Row>
+                        <Row style={{height:"60%"}}>
+                        </Row>
+                        <Row style={{borderTop:"1px black solid"}}>
+                            <div style={{display:"flex",justifyContent:"center",width:"100%",color:"#111"}}><p>Parameters</p></div>
+                            
+                            <div style={{width:"100%"}}>
+                                <Row style={{marginBottom:"0.5em"}}>
+                                    <Col span={8} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                        Ethnicity :
+                                    </Col>
+                                    <Col span={16}>
+                                        <Select placeholder="Ethnicity" style={{width:"100%"}}>
+                                            <Option value="Asian">Asian</Option>
+                                            <Option value="Hispanic">Hispanic</Option>
+                                            <Option value="White">White</Option>
+                                            <Option value="Nordic">Nordic</Option>
+                                        </Select>
+                                    </Col>
+                                </Row>
+                                <Row style={{marginBottom:"0.5em"}}>
+                                    <Col span={8} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                        Gender :
+                                    </Col>
+                                    <Col span={16}>
+                                        <Select placeholder="Gender" style={{width:"100%"}}>
+                                            <Option value="Male">Male</Option>
+                                            <Option value="Female">Female</Option>
+                                        </Select>
+                                    </Col>
+                                </Row>
+                                <Row style={{marginBottom:"0.5em"}}>
+                                    <Col span={8} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                        Fitness :
+                                    </Col>
+                                    <Col span={16}>
+                                        <Slider defaultValue={Math.random()*100} style={{margin:"0.5em"}}/>
+                                    </Col>
+                                </Row>
+                                <Row style={{marginBottom:"0.5em"}}>
+                                    <Col span={8} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                        Skintone :
+                                    </Col>
+                                    <Col span={16}>
+                                        <Slider defaultValue={Math.random()*100} style={{margin:"0.5em"}}/>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Row>
                     </div>
                 </Col>
             </Row>
