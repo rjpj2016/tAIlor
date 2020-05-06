22a85d780dfdb3fb98d87ec17dbc28361ed6019c
Rajat Parajuli <rjpj@parajuli.me>
2020-05-06 14:39:46 +0545

    Added a rotating human to model picker example

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Img from 'gatsby-image';
 import { Row,Col, Button } from "antd";
 
 import TexturePicker from '../components/texturepicker';
+import ModelPicker from '../components/modelpicker';
 
 const dummy = () => (<div/>)
 
@@ -18,8 +19,8 @@ const Stages = {
         app: TexturePicker
     },
     MODEL_PICKER: {
-        description: "Select closest model",
-        app: dummy
+        description: "Select Model",
+        app: ModelPicker
     },
     INTERACTOR: {
         description: "Interactive Demo",
@@ -46,22 +47,22 @@ export default ({data}) => {
     return (<TailorLayout>
         <div className="mainScreen">
             <Row className="stageSelector">
-                <Col span={4}>
+                <Col xs={6} md={4}>
                     <Button type="primary" block  disabled={stage == 0} onClick={ () => {setStage(stage - 1 )}}>
                         Previous
                     </Button>
                 </Col>
-                <Col span={16} className="stageInfo">
+                <Col xs={12} md={16} className="stageInfo">
                     {StagesOrder[stage].description}
                 </Col>
-                <Col span={4}>
+                <Col xs={6} md={4}>
                     <Button type="primary" block disabled={stage == (StagesOrder.length-1) } onClick={ () => {setStage(stage + 1 )}}>
                         Next
                     </Button>
                 </Col>
             </Row>
             <Row className="appStage">
-                <SubApp textureState={[texture,setTexture]}/>
+                <SubApp textureState={[texture,setTexture]} modelState={[model,setModel]}/>
             </Row>
 
             
